5136dc8049f77a772d8ee20a24fe0de91b1e5eea
Alejandro Raffo <amsterdamAL@gmail.com>
2021-03-09 16:30:23 -0500

    added grid to header components

--- app/javascript/packs/header.jsx
+++ app/javascript/packs/header.jsx
@@ -5,7 +5,7 @@ import {Link, useLocation} from 'react-router-dom'
 //import useDocumentScrollThrottled from './useDocumentScrollThrottled.jsx'
 import styled from 'styled-components'
 import headerLeaf from "../../assets/images/headerLeaf.svg"
-//import Logo from '../../assets/images/cwaLogo5.jpg'
+import headerLogo from '../../assets/images/logo.png'
 
 //import Burger from './burger'
 //import SideMenu from './sidemenu'
@@ -13,20 +13,30 @@ import headerLeaf from "../../assets/images/headerLeaf.svg"
 
 const HeaderWrapper = styled.div`
 
-/* background-image: url(${headerLeaf});
-background-position: right;
-background-repeat: no-repeat;
-background-size: contain;
-min-height: 85px;
-min-width: 80vw; */
+    /* background-image: url(${headerLeaf});
+    background-position: right;
+    background-repeat: no-repeat;
+    background-size: contain;
+    min-height: 85px;
+    min-width: 80vw; */
 
 
-min-height: 85px;
-min-width: 600px;
-background: pink;
+    min-height: 85px;
+    width: 100%;
+    background: pink;
+
+    display: grid;
+    grid-template-columns: minmax(75px, 20vw) minmax(300px, 80vw);
+    grid-template-rows: 85px;
+
+    grid-template-areas:
+
+        "headerLogo headerLeaf"
+        "headerLogo headerLeaf";
+
 `;
 
-const Logo = styled.h1`
+const Logo = styled.img`
 
     font-family: 'Poppins', sans-serif;
     font-style: normal;
@@ -36,21 +46,63 @@ const Logo = styled.h1`
     letter-spacing: -5px;
     /* identical to box height */
 
+    grid-area: headerLogo;
+    width: 80%;
 
-color: #010101;
+    justify-self: end;
+    align-self: center;
+    min-width: 70px;
+
+    color: #010101;
 
 
 `;
 
 const HeaderLeafImage = styled.img`
 
-    max-width: 80vw;
-    min-height: 100%;
-    float: right;
-
+    max-width: 100%;
+    //min-height: 100%;
+    
+    grid-area: headerLeaf;
+    justify-self: end;
+    align-self: center;
 
 `;
 
+const Nav = styled.nav`
+
+    grid-area: headerLeaf;
+    color: white;
+    align-self: center;
+    justify-self: end;
+    
+
+    ul{
+        display: flex;
+        align-items: flex-end;
+        li{
+            list-style-type: none;
+            margin-right: 40px;
+            font-family: Poppins;
+            font-style: normal;
+            font-weight: normal;
+            
+            line-height: 72px;
+            /* identical to box height */
+
+
+            color: #FFFFFF;
+
+        }
+
+
+
+    }
+    
+
+
+
+`;
 
 
 function Header(props) {
@@ -64,8 +116,18 @@ function Header(props) {
     return (
         
         <HeaderWrapper>
-                <Logo>LOGO</Logo>
+                <Logo src={headerLogo}></Logo>
                 <HeaderLeafImage src={headerLeaf}></HeaderLeafImage>
+                <Nav>
+                    <ul>
+                        <li>news</li>
+                        <li>act</li>
+                        <li>shop</li>
+
+                    </ul>
+
+
+                </Nav>
             
         
         
--- app/javascript/packs/home.jsx
+++ app/javascript/packs/home.jsx
@@ -15,7 +15,7 @@ function Home(props){
     
     return (
         <>
-            <h1>homeComponent</h1>
+            
         </>
     );  
 }
