fb80009361da245c5e66ce6c598c5c9666e150bb
Alejandro Raffo <amsterdamAL@gmail.com>
2021-06-05 13:04:53 -0400

    uri error

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,20 +1,15 @@
 class Story < ApplicationRecord
 
+    require "addressable/uri"
+
+
     # frozen_string_literal: true
 
     # Sets the <tt>ActiveStorage::Current.host</tt> attribute, which the disk service uses to generate URLs.
     # Include this concern in custom controllers that call ActiveStorage::Blob#url,
     # ActiveStorage::Variant#url, or ActiveStorage::Preview#url so the disk service can
     # generate URLs using the same host, protocol, and base path as the current request.
-    module ActiveStorage::SetCurrent
-        extend ActiveSupport::Concern
     
-        included do
-        before_action do
-            ActiveStorage::Current.host = request.base_url
-        end
-        end
-    end
 
 
 
@@ -191,13 +186,18 @@ class Story < ApplicationRecord
 
         puts "------------after_validation callback begin -------------------"
         
-            
+        puts "is self image attached?"
         if self.image.attached?
 
-            puts "URL IS = " + self.image.url
-            url = self.image.url&.split("?")&.first
-        
-            puts "self = " + self.title
+            
+            puts "yes it is, start to split url"
+
+            puts "url to split is " + self.image.url.to_s
+            url = self.image.url.split("?").first
+            puts "final selfurl is " + url.to_s
+
+            
+            puts "self title= " + self.title
         
         
         
